2ee706603dc6830d7697c4e9fd4edb9315f5b136
Sinhuè Rossi <sinhue.rossi@gmail.com>
2019-11-07 12:06:56 +0100

    update db

--- arduino/main.py
+++ arduino/main.py
@@ -1,8 +1,10 @@
 import pyfirmata
 import os
+import win32api
+import requests
 import time
-import random
-import pyautogui
+import subprocess
+import win32gui
 
 # se passa abbastanza tempo senza far nulla, si fa partire un video completamente nero che fa da screensaver
 
@@ -13,6 +15,8 @@ it = pyfirmata.util.Iterator(board)
 it.start()
 
 # init vari
+# si fanno partire VLC e Firefox
+
 
 
 #pins
@@ -20,12 +24,13 @@ ledPin = 13
 sensorPin = board.get_pin('d:4:i')
 
 # variables
-val = 0
-motionState = False
+val = False
 
-
+# occhio, aggiungere interfaccia web per VLC e usare Firefox Beta (ha kiosk mode)
+vlc = subprocess.Popen(['vlc.exe', '-f', '--start-paused', '--extraintf', 'http', '--intf', 'wx', 'C:\\Users\\Usuario\\Videos\\GreenScreenFireFootage[1].mp4'], executable="C:\\Program Files\\VideoLAN\\VLC\\vlc.exe", shell=False)
+firefox = None
 # bisogna aspettare che si inizializzi il sensore
-time.sleep(65)
+#time.sleep(65)
 
 while True:
     val = sensorPin.read()
@@ -33,12 +38,13 @@ while True:
 
     if val:
         board.digital[ledPin].write(1)
-        os.system('cmd /c \" "C:\\Program Files\\VideoLAN\\VLC\\vlc.exe" --fullscreen "C:\\Users\\Usuario\\Videos\\GreenScreenFireFootage[1].mp4"')
-        if motionState is False:
-            motionState = True
+        requests.get('http://127.0.0.1:8080/requests/status.xml?command=pl_stop')
+        if firefox == None:
+            firefox = subprocess.Popen(['firefox.exe', 'www.google.it', '--kiosk'], executable='C:\\Program Files\\Mozilla Firefox\\firefox.exe', shell=False)
     else:
         board.digital[ledPin].write(0)
-        if motionState is True:
-            motionState = False
-    time.sleep(1)
+        if firefox != None:
+            firefox = None
+        requests.get('http://127.0.0.1:8080/requests/status.xml?command=pl_play')
+    time.sleep(5)
 board.exit()
