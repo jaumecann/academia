7df7af4205d4b0c0b8b3740d5fc78e40b6b7b337
Jaume <jaumecann@gmail.com>
2019-11-18 19:11:13 +0100

    milllora fotos, clicks per handicapped a immagine

--- php/conexion.php
+++ php/conexion.php
@@ -1,3 +1,4 @@
 <?php
-$conn = new PDO('mysql:host=192.168.1.39;dbname=accademia;charset=utf8','accademia','8JEMCpc5m2yMxbN5')
+//$conn = new PDO('mysql:host=192.168.1.39;dbname=accademia;charset=utf8','accademia','8JEMCpc5m2yMxbN5')
+$conn = new PDO('mysql:host=localhost;dbname=accademia;charset=utf8','root','1234')
 ?>
