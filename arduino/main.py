a05aab100a3429f3c9db9abf17c08172048e11f9
pingumen96 <sinhue.rossi@gmail.com>
2019-11-20 14:02:38 +0100

    modifiche

--- arduino/main.py
+++ arduino/main.py
@@ -5,67 +5,76 @@ import requests
 import time
 import subprocess
 
+# VLC va impostato per avere una istanza sola
+
 class State(Enum):
+    INIT = 0
     SCREENSAVER = 1
     VIDEO = 2
     LIGHTS = 3
 
-currentState = State.SCREENSAVER
+currentState = State.INIT
 
 # Arduino init
-#port = 'COM7'
-#board = pyfirmata.Arduino(port)
-#it = pyfirmata.util.Iterator(board)
-#it.start()
+port = '/dev/ttyACM0'
+board = pyfirmata.Arduino(port)
+it = pyfirmata.util.Iterator(board)
+it.start()
 
 # init vari
-# si fanno partire VLC e Firefox
-vlc = subprocess.Popen(["vlc", "-f", "--start-paused", "--extraintf", "http", "--http-host", "localhost", "--http-port", "9999", "/home/adminuc/Videos/NotizieallaBreaking Italy-dSPn2fsmbzQ.webm"])
-firefox = subprocess.Popen(["firefox", "--kiosk", "www.google.it"])
+# si fanno partire VLC e Chrome
+chrome = subprocess.Popen(["google-chrome", "--kiosk", "www.google.it"])
 
 
 time.sleep(5)
 #pins
-#ledPin = 13
-#sensorPin = board.get_pin('d:4:i')
+ledPin = 13
+sensorPin = board.get_pin('d:4:i')
 
 # variables
-#val = False
-
-# occhio, aggiungere interfaccia web per VLC e usare Firefox Beta (ha kiosk mode)
+val = False
+disableSensor = False
+playing = False
+chromeForeground = False
+vlcForeground = False
+vlc = None
 # bisogna aspettare che si inizializzi il sensore
 #time.sleep(65)
 
 while True:
     # si controlla se è il caso di cambiare stato
-
-    if currentState == State.SCREENSAVER:
+    if currentState == State.INIT:
+        vlc = subprocess.Popen(["vlc", "-f", "--no-start-paused", "--extraintf", "http", "--http-host", "localhost", "--http-port", "9999", "/home/adminuc/Videos/NotizieallaBreaking Italy-dSPn2fsmbzQ.webm"])
+        time.sleep(3)
         print(vlc.pid)
+        if not playing:
+            requests.get('http://localhost:9999/requests/status.xml?command=pl_play')
+            requests.get('http://localhost:9999/requests/status.xml?command=pl_loop')
+            playing = True
 
-        requests.get('http://localhost:9999/requests/status.xml?command=pl_play')
 
         # si piazza vlc di fronte a tutto
-        os.system("xdotool windowactivate `xdotool search --pid " + vlc.pid + " | tail -1`")
-        pass
+        os.system("xdotool windowactivate `xdotool search --pid " + str(vlc.pid) + " | tail -1`")
+        currentState = State.SCREENSAVER
+    elif currentState == State.SCREENSAVER:
+        # si controlla il sensore
+        val = sensorPin.read()
+
+        if val and not disableSensor:
+            disableSensor = True
+            currentState = State.VIDEO
+            os.system("vlc -f --no-start-paused /home/adminuc/Videos/2.mkv")
+            time.sleep(2)
+            requests.get('http://localhost:9999/requests/status.xml?command=pl_play')
+            currentState = State.VIDEO
     elif currentState == State.VIDEO:
+        time.sleep(10)
+        currentState = State.LIGHTS
         pass
     elif currentState == State.LIGHTS:
-        os.system("xdotool windowactivate `xdotool search --pid " + firefox.pid + " | tail -1`")
-        pass
-    #val = sensorPin.read()
-    #print(val)
+        if not chromeForeground:
+            os.system("wmctrl -a 'Google Chrome'")
+            chromeForeground = True
+        time.sleep(10)
 
-    #if val:
-    #    board.digital[ledPin].write(1)
-    #    requests.get('http://127.0.0.1:8080/requests/status.xml?command=pl_stop')
-    #    if firefox == None:
-    #        Application().start("C:\\Program Files\\Mozilla Firefox\\firefox.exe --kiosk www.google.it")
-    #        firefox = Desktop(backend="uia").MozillaFirefox
-            #firefox = subprocess.Popen(['firefox.exe', 'www.google.it', '--kiosk'], executable='C:\\Program Files\\Mozilla Firefox\\firefox.exe', shell=False)
-    #else:
-    #    board.digital[ledPin].write(0)
-    #    if firefox != None:
-    #        firefox.minimize()
-    #    requests.get('http://127.0.0.1:8080/requests/status.xml?command=pl_play')
-    #time.sleep(2)
-#board.exit()
+board.exit()
